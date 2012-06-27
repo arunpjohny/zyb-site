4ecd4243764d89ed02dffe84db7b1b8528f7fe79
Arun P Johny <arun.official.mail@gmail.com>
2012-06-27 21:55:07 +0530

    Manager: Added service method to fetch bytes from model objects

--- src/java/in/co/zybotech/core/service/Manager.java
+++ src/java/in/co/zybotech/core/service/Manager.java
@@ -1,5 +1,7 @@
 package in.co.zybotech.core.service;
 
+import in.co.zybotech.model.career.Student;
+
 import java.io.Serializable;
 
 public interface Manager {
@@ -7,5 +9,9 @@ public interface Manager {
 	<T> T getObject(Class<T> clazz, Serializable id);
 
 	void saveObject(Object object);
+
+	byte[] getBytes(Class<Student> clazz, Serializable id, String field)
+			throws SecurityException, NoSuchFieldException,
+			IllegalArgumentException, IllegalAccessException;
 
 }
--- src/java/in/co/zybotech/core/service/impl/BaseManager.java
+++ src/java/in/co/zybotech/core/service/impl/BaseManager.java
@@ -1,12 +1,16 @@
 package in.co.zybotech.core.service.impl;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
 
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ReflectionUtils;
 
 import in.co.zybotech.core.dao.DAO;
+import in.co.zybotech.core.exception.client.ResourceNotFoundException;
 import in.co.zybotech.core.service.Manager;
+import in.co.zybotech.model.career.Student;
 
 @Transactional(readOnly = true, propagation = Propagation.REQUIRED)
 public class BaseManager implements Manager {
@@ -26,6 +30,24 @@ public class BaseManager implements Manager {
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public void saveObject(Object object) {
 		dao.saveObject(object);
+	}
+
+	@Override
+	public byte[] getBytes(Class<Student> clazz, Serializable id, String field)
+			throws SecurityException, NoSuchFieldException,
+			IllegalArgumentException, IllegalAccessException {
+		Student object = getObject(clazz, id);
+		if (object == null) {
+			throw new ResourceNotFoundException("Unable to find the resource "
+					+ clazz + " with id " + id + ".");
+		}
+		Field f = ReflectionUtils.findField(clazz, field);
+		if (f == null) {
+			throw new ResourceNotFoundException("Unable to find field" + f
+					+ " from " + clazz + ".");
+		}
+		f.setAccessible(true);
+		return (byte[]) f.get(object);
 	}
 
 }
