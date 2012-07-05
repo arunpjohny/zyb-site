d643375943b0534a5e15d58b09d62fa54b4130cb
Arun P Johny <arun.official.mail@gmail.com>
2012-07-05 23:28:02 +0530

    MailManager: Allows to send attachments

--- src/java/in/co/zybotech/service/MailManager.java
+++ src/java/in/co/zybotech/service/MailManager.java
@@ -2,8 +2,14 @@ package in.co.zybotech.service;
 
 import in.co.zybotech.core.service.Manager;
 
+import java.util.Map;
+
 public interface MailManager extends Manager {
 
 	void sendMail(String from, String to, String subject, String body);
+
+	public abstract void sendMail(final String from, final String to,
+			final String subject, final String message,
+			final Map<String, byte[]> attachments);
 
 }
--- src/java/in/co/zybotech/service/impl/MailManagerImpl.java
+++ src/java/in/co/zybotech/service/impl/MailManagerImpl.java
@@ -1,21 +1,28 @@
 package in.co.zybotech.service.impl;
 
+import in.co.zybotech.core.service.impl.BaseManager;
+import in.co.zybotech.service.MailManager;
+
+import java.util.Map;
+
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import in.co.zybotech.core.service.impl.BaseManager;
-import in.co.zybotech.service.MailManager;
 
 @Service
 @Transactional(readOnly = true, propagation = Propagation.REQUIRED)
 public class MailManagerImpl extends BaseManager implements MailManager {
 
 	@Autowired
-	private MailSender mailSender;
+	private JavaMailSender mailSender;
 
 	@Override
 	public void sendMail(String from, String to, String subject, String body) {
@@ -28,4 +35,26 @@ public class MailManagerImpl extends BaseManager implements MailManager {
 		mailSender.send(simpleMessage);
 	}
 
+	@Override
+	public void sendMail(final String from, final String to,
+			final String subject, final String message,
+			final Map<String, byte[]> attachments) {
+
+		MimeMessagePreparator preparator = new MimeMessagePreparator() {
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				MimeMessageHelper messageHelper = new MimeMessageHelper(
+						mimeMessage, true, "UTF-8");
+				messageHelper.setSubject(subject);
+				messageHelper.setTo(to);
+				messageHelper.setFrom(from);
+				messageHelper.setText(message);
+				for (Map.Entry<String, byte[]> entry : attachments.entrySet()) {
+					messageHelper.addAttachment(entry.getKey(),
+							new ByteArrayResource(entry.getValue()));
+				}
+
+			}
+		};
+		mailSender.send(preparator);
+	}
 }
